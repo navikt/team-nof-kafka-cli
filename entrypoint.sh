0c6505a32a30ab3b7c6f0aa51fa36facbdc68265
Robin <robin.tordly@nav.no>
2022-08-03 09:36:48 +0200

    use other apps kafka cred

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,6 @@
 
 if [[ -z "$KAFKA_BROKERS" ]]; then
     echo "missing required variable KAFKA_BROKERS"
-else
-  KAFKA_BROKERS=$(cat /var/run/secrets/KAFKA_BROKERS)
-  export KAFKA_BROKERS
-fi
-
-if [[ -z "$KAFKA_KEYSTORE_PATH" ]]; then
-    echo "missing required variable KAFKA_KEYSTORE_PATH"
-fi
-
-if [[ -z "$KAFKA_TRUSTSTORE_PATH" ]]; then
-    echo "missing required variable KAFKA_TRUSTSTORE_PATH"
 fi
 
 if [[ -z "$KAFKA_CREDSTORE_PASSWORD" ]]; then
@@ -22,9 +11,9 @@ fi
 echo "
 security.protocol=SSL
 ssl.keystore.type=PKCS12
-ssl.keystore.location=/var/run/secrets/client.keystore.p12
+ssl.keystore.location=/var/run/secrets/other-app/kafka/client.keystore.p12
 ssl.keystore.password=$KAFKA_CREDSTORE_PASSWORD
-ssl.truststore.location=/var/run/secrets/client.truststore.jks
+ssl.truststore.location=/var/run/secrets/other-app/kafka/client.truststore.jks
 ssl.truststore.password=$KAFKA_CREDSTORE_PASSWORD
 " >> /cli/kafka/config/aiven.conf
 
